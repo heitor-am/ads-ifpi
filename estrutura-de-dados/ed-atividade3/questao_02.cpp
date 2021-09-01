408cdb001c967c052b8deb7940ab36fadded3a25
Heitor Moura <heitorandrade2001@gmail.com>
2021-08-31 23:42:10 -0300

    ✅ problem 2 solved

---
+++ estrutura-de-dados/ed-atividade3/questao_02.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+#define n 5
+
+int main() {
+  int a[n], b[n], c[n * 2];
+
+  cout << "### 1° VETOR ### " << endl;
+  for (int i = 0; i < n; i++) {
+    cout << "Digite o " << i + 1 << "° valor: ";
+    cin >> a[i];
+  }
+
+  cout << "\n### 2° VETOR ### " << endl;
+  for (int i = 0; i < n; i++) {
+    cout << "Digite o " << i + 1 << "° valor: ";
+    cin >> b[i];
+  }
+
+  for (int i = 0; i < n; i++) {
+    c[i] = a[i];
+    c[i + n] = b[i];
+  }
+
+  cout << "\nResultado: ";
+  for (int i = 0; i < (n * 2); i++) {
+    cout << c[i] << " ";
+  }
+  cout << endl;
+}

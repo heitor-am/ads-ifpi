31be6a3bd76bbf5951c104c3064855abfb5fe37a
Heitor Moura <heitorandrade2001@gmail.com>
2021-08-31 23:43:13 -0300

    ✅ problem 6 solved

---
+++ estrutura-de-dados/ed-atividade3/questao_06.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, array[n] = {0, 1};
+
+  cout << "Informe a quantidade de termos: ";
+  cin >> n;
+
+  for (int i = 1; i < n; i++) {
+    array[i + 1] = array[i] + array[i - 1];
+  }
+
+  cout << "\nResultado: ";
+  for (int i = 0; i < n; i++) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}

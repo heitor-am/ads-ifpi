e3147c203906c0f494b14cbdfe9386939c853742
Heitor Moura <heitorandrade2001@gmail.com>
2021-08-31 23:42:56 -0300

    ✅ problem 5 solved

---
+++ estrutura-de-dados/ed-atividade3/questao_05.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+#define n 5
+
+int main() {
+  int array[n], biggest, smallest, positionOfBiggest, positionOfSmallest;
+
+  for (int i = 0; i < n; i++) {
+    cout << "Digite o " << i + 1 << "° valor: ";
+    cin >> array[i];
+  }
+
+  biggest = array[0];
+  smallest = array[0];
+
+  for (int i = 0; i < n; i++) {
+    if (array[i] > biggest) {
+      biggest = array[i];
+      positionOfBiggest = i;
+    }
+    if (array[i] < smallest) {
+      smallest = array[i];
+      positionOfSmallest = i;  
+    }
+  }
+
+  cout << "\nMaior: " << biggest << " | Posição: " << positionOfBiggest << endl;
+  cout << "Menor: " << smallest << " | Posição: " << positionOfSmallest << endl;
+}

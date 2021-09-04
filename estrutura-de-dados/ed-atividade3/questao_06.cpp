cdd2422718adc4de16be5eeb22f70fbe6b6c9c51
Heitor Moura <heitorandrade2001@gmail.com>
2021-09-03 23:29:41 -0300

    ✅ problem 6 review

--- estrutura-de-dados/ed-atividade3/questao_06.cpp
+++ estrutura-de-dados/ed-atividade3/questao_06.cpp
@@ -7,13 +7,13 @@ int main() {
   cout << "Informe a quantidade de termos: ";
   cin >> n;
 
-  for (int i = 1; i < n; i++) {
-    array[i + 1] = array[i] + array[i - 1];
+  for (int i = 0; i < n; i++) {
+    array[i + 2] = array[i] + array[i + 1];
   }
 
-  cout << "\nResultado: ";
+  cout << "\nResultado:";
   for (int i = 0; i < n; i++) {
-    cout << array[i] << " ";
+    cout << " " << array[i];
   }
   cout << endl;
 }
